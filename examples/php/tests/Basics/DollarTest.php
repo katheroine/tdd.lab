02421f7089ca974534b549ba87b7b4161f002274
katheroine <katheroine@gmail.com>
2026-07-27 21:18:26 +0200

    DollarTest: testEquality: Error: Call to undefined method TddLab\Basics\Dollar::equals()

--- examples/php/tests/Basics/DollarTest.php
+++ examples/php/tests/Basics/DollarTest.php
@@ -17,4 +17,9 @@ class DollarTest extends TestCase
         $product = $five->multiply(3);
         $this->assertEquals(15, $product->amount);
     }
+
+    public function testEquality()
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+    }
 }
