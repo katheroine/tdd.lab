502016877e901540f63c301ea8dc3eed49d3668c
katheroine <katheroine@gmail.com>
2026-08-05 21:11:13 +0200

    MoneyTest: testCurrencyInequality: Fail: Failed asserting that true is false.

---
+++ examples/php/tests/Basics/MoneyTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace TddLab\Basics;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    #[Test]
+    public function testCurrencyInequality()
+    {
+        $this->assertFalse((new Dollar(5))->equals(new SwissFranc(5)));
+        $this->assertFalse((new SwissFranc(5))->equals(new Dollar(5)));
+    }
+}
